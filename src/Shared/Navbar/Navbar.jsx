9e863427a27c5694ce761e6c8219ac039a38d98b
Sarwarhridoy4 <sarwarhidoy4@gmail.com>
2023-04-09 21:48:14 +0600

    added sidebar

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
             Flowbite
           </span> */}
         </Link>
-        <div class="text-center">
+        {/* <div class="text-center">
           <button
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
             type="button"
@@ -30,7 +30,7 @@ const Navbar = () => {
           >
             Show drawer without backdrop
           </button>
-        </div>
+        </div> */}
         <div class="flex items-center md:order-2">
           <button
             type="button"
