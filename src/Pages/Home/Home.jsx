a06463c8a69853e51fa0fde1214843c65b9f9664
Sarwarhridoy4 <sarwarhidoy4@gmail.com>
2023-04-13 13:21:50 +0600

    Lecture card Added in LMS Home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import PreQuestionire from "./PreQuestionire/PreQuestionire";
+import LectureScedule from "./LectureScedule/LectureScedule";
 
 
 const Home = () => {
   return (
     <div className="">
-      <PreQuestionire />
+      {/* <PreQuestionire /> */}
+      <LectureScedule/>
+      <LectureScedule/>
     </div>
   );
 };
