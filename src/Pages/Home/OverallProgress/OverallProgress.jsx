1f34131ec5e16d911d6cbf0bc1d10422ec0f4a8c
Sarwarhridoy4 <sarwarhidoy4@gmail.com>
2023-04-15 15:55:20 +0600

    attendance added

--- src/Pages/Home/OverallProgress/OverallProgress.jsx
+++ src/Pages/Home/OverallProgress/OverallProgress.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import DoughnutChart from '../../../Components/DoughnutChart/DoughnutChart';
 import QuizChart from '../../../Components/QuizChart/QuizChart';
+import AttendanceChart from '../../../Components/AttendanceChart/AttendanceChart';
 
 const OverallProgress = () => {
   return (
     <div className='my-10'>
-      <div className="container w-full mx-auto flex flex-col items-center md:flex-row">
+      <div className="w-4/5 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         <div>
         <h3 className="text-3xl font-poppins font-medium">Overall Progress</h3>
         <DoughnutChart/>
@@ -13,6 +14,10 @@ const OverallProgress = () => {
         <div>
         <h3 className="text-3xl font-poppins font-medium">Quizes</h3>
         <QuizChart/>
+        </div>
+        <div>
+        <h3 className="text-3xl font-poppins font-medium">Attandance</h3>
+        <AttendanceChart/>
         </div>
         </div>
     </div>
