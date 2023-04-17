32537f906a8730123992489a58939e918d9cd38e
Sarwarhridoy4 <sarwarhidoy4@gmail.com>
2023-04-17 16:40:02 +0600

    redefined input width

--- src/Pages/DashBoard/AdminPannel/AddLectures/AddLectures.jsx
+++ src/Pages/DashBoard/AdminPannel/AddLectures/AddLectures.jsx
@@ -107,8 +107,8 @@ const AddLectures = () => {
             <option value='.mp4'>Video</option>
           </select>
           {fileType && (
-            <div className='my-10'>
-              <div class='flex w-full items-center justify-center bg-grey-lighter'>
+            <div className='my-4'>
+              <div class='flex w-64 items-center justify-center bg-grey-lighter'>
                 <label class='w-64 flex flex-col items-center px-4 py-6 bg-green-400 text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-white'>
                   <svg
                     class='w-8 h-8'
@@ -119,7 +119,7 @@ const AddLectures = () => {
                     <path d='M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z' />
                   </svg>
                   <span class='mt-2 text-base leading-normal'>
-                    Select a file
+                    {`Select a ${fileType} file`}
                   </span>
 
                   <input
