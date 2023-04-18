fcb3076c89968414c8d6ab4e244f105dba94ad59
Sarwarhridoy4 <sarwarhidoy4@gmail.com>
2023-04-18 15:24:37 +0600

    justify-center added

--- src/Pages/DashBoard/Analysis/OverView/OverView.jsx
+++ src/Pages/DashBoard/Analysis/OverView/OverView.jsx
@@ -6,17 +6,17 @@ const OverView = () => {
     <div className="mt-24">
       <h4 className="text-2xl font-poppins font-medium">Overview</h4>
       <div className='flex flex-col md:flex-row justify-between border border-1 border-[#D4D4D4] rounded-lg'>
-        <div className="flex flex-col items-center p-12">
+        <div className="flex flex-col items-center justify-center p-12">
           <h4 className="text-2xl font-poppins">Above Average</h4>
           <p className="font-poppins font-medium">Rating</p>
       </div>
       <div className="h-16 border border-1 my-auto"></div>
-        <div className="flex flex-col items-center p-12">
+        <div className="flex flex-col items-center justify-center p-12">
           <h4 className="text-2xl font-poppins">85%</h4>
           <p className="font-poppins font-medium">Percentile</p>
       </div>
       <div className="h-16 border border-1 my-auto"></div>
-        <div className="flex flex-col items-center p-12">
+        <div className="flex flex-col items-center justify-center p-12">
           <h4 className="text-2xl font-poppins">80</h4>
           <p className="font-poppins font-medium">Overall rank</p>
       </div>
