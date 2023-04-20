9831054de8e8aaf7b774800a28a69ef749f61a67
amitpaultl <amitpaultl@gmail.com>
2023-04-20 12:57:20 +0600

    video add

--- src/Pages/DashBoard/MyCourses/SpecificCourse/EachModule/EachModule.jsx
+++ src/Pages/DashBoard/MyCourses/SpecificCourse/EachModule/EachModule.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-
 const EachModule = ({ module }) => {
   return (
-    <div>
-      <h1>EachModule</h1>
+    <div className="text-center border p-5 rounded-sm hover:bg-green-400 hover:rounded-sm">
+      <h1 className="font-semibold text-2xl">{module?.moduleName}</h1>
+      <h3 className="my-2">{module?.instructor}</h3>
+      <div className="flex justify-between">
+        <span>  <b>Time </b>: {module?.time}</span>
+        <span>  <b>Lecture </b>: {module?.totalLecture}</span>
+      </div>
     </div>
   );
 };
