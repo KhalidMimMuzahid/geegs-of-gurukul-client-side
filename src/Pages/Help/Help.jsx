108f184ce5e69a6a5207c99e1cc38a3bb400ca2f
Abdullah Al Noman <khubaibulislam2001@gmail.com>
2023-05-02 15:27:13 +0600

    Previews tickets completed

--- src/Pages/Help/Help.jsx
+++ src/Pages/Help/Help.jsx
@@ -1,23 +1,52 @@
-import React from 'react'
-import { Tab, TabList, TabPanel, Tabs } from 'react-tabs'
+import React, { useState } from 'react'
 import 'react-tabs/style/react-tabs.css';
+ import ticket from '../../assets/ticket/ticket.svg';
 
 const Help = () => {
-  return (
-    <div className='tabTocken'>
-      <Tabs>
-    <TabList>
-      <Tab>Title 1</Tab>
-      <Tab>Title 2</Tab>
-    </TabList>
+  const [ticketStatus, setTicketStatus] = useState("Create new ticket");
 
-    <TabPanel>
-      <h2>Any content 1</h2>
-    </TabPanel>
-    <TabPanel>
-      <h2>Any content 2</h2>
-    </TabPanel>
-  </Tabs>
+
+  return (
+    <div className=" bg-[#C4F0CE] h-screen">
+      <div 
+      
+      className='bg-white w-[90vw] h-4/5 mx-auto my-8 rounded-xl pt-10'
+    >
+
+      <div className='flex justify-evenly font-poppins text-xl'>
+        <button 
+          onClick={()=>setTicketStatus("Create new ticket")}
+          className={`${ticketStatus === "Create new ticket" && "border-b-[3px] border-[#0ABD67]"}`}
+        >
+          Create new ticket
+        </button>
+
+        <div className='h-10 w-[0.5px] bg-black'></div>
+
+        <button 
+          onClick={()=>setTicketStatus("Previews ticket")} className={`${ticketStatus === "Previews ticket" && "border-b-[3px] border-[#0ABD67]"}`}
+        >
+          Previews ticket
+        </button>
+      </div>
+
+      {
+        ticketStatus === "Previews ticket" && 
+        <div 
+          style={{ backgroundImage: `url(${ticket})` }} 
+          className='flex flex-col justify-center items-center gap-y-8 h-full bg-center bg-no-repeat'
+        >
+          <h3 className='text-xl'>You don't have any previous ticket</h3>
+          <button 
+            onClick={()=>setTicketStatus("Create new ticket")}
+            className='py-5 px-4 bg-[#0ABD67] text-white text-xl rounded-2xl font-semibold tracking-wide'
+          >
+            Create new ticket
+          </button>
+        </div>
+      }
+
+    </div>
     </div>
   )
 }
