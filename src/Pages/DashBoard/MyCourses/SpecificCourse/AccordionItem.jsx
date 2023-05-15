73e7196bae13a78cb1370570f7c73b2987a40a93
JAYENDRA06 <jayendra.awasthi2020@vitstudent.ac.in>
2023-05-15 12:10:56 +0530

    update: next and prev buttons

--- src/Pages/DashBoard/MyCourses/SpecificCourse/AccordionItem.jsx
+++ src/Pages/DashBoard/MyCourses/SpecificCourse/AccordionItem.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { BsFillCheckCircleFill, BsFillCaretRightFill } from "react-icons/bs";
 
-function AccordionItem({ content, setSelectedLecture  }) {
+function AccordionItem({ content, setSelectedLecture }) {
   const [isOpen, setIsOpen] = useState(false);
   const { moduleName, lecturesList } = content;
   return (
@@ -18,17 +18,17 @@ function AccordionItem({ content, setSelectedLecture  }) {
         />
       </button>
 
-      <div className= {`${isOpen && "p-4"}`}>
+      <div className={`${isOpen && "p-4"}`}>
         {isOpen && (
           <>
             {lecturesList.map((lecture) => (
               <button
                 type="button"
                 className="w-full px-4 py-2 my-1 text-left border border-gray-200 bg-gray-50 flex items-center focus:bg-green-100"
-                onClick={() =>{
-                  setSelectedLecture(lecture)
-
-                } }
+                onClick={() => {
+                  setSelectedLecture(lecture);
+                }}
+                key={lecture._id}
               >
                 <BsFillCheckCircleFill color="green" className="flex-none" />
                 <p className="flex-1 pl-3">{lecture.lectureName}</p>
--- src/Pages/DashBoard/MyCourses/SpecificCourse/ContentDetails/ContentDetails.jsx
+++ src/Pages/DashBoard/MyCourses/SpecificCourse/ContentDetails/ContentDetails.jsx
@@ -1,18 +1,70 @@
 import React from "react";
 
-function ContentDetails({ description, selectedLecture }) {
-  console.log("selectedLecture: ", selectedLecture);
+function ContentDetails({
+  description,
+  selectedLecture,
+  contents,
+  setSelectedLecture,
+}) {
+  const prevLecture = () => {
+    const moduleIndex = contents.findIndex(
+      (element) => element._id === selectedLecture.module_id
+    );
+    const lectureIndex = contents[moduleIndex].lecturesList.findIndex(
+      (element) => element._id === selectedLecture._id
+    );
+    try {
+      if (lectureIndex === 0) {
+        setSelectedLecture(
+          contents[moduleIndex - 1].lecturesList[
+            contents[moduleIndex - 1].lecturesList.length - 1
+          ]
+        );
+      } else {
+        setSelectedLecture(
+          contents[moduleIndex].lecturesList[lectureIndex - 1]
+        );
+      }
+    } catch {
+      console.log("start reached");
+    }
+  };
+  const nextLecture = () => {
+    const moduleIndex = contents.findIndex(
+      (element) => element._id === selectedLecture.module_id
+    );
+    const lectureIndex = contents[moduleIndex].lecturesList.findIndex(
+      (element) => element._id === selectedLecture._id
+    );
+    try {
+      if (lectureIndex === contents[moduleIndex].lecturesList.length - 1) {
+        setSelectedLecture(contents[moduleIndex + 1].lecturesList[0]);
+      } else {
+        setSelectedLecture(
+          contents[moduleIndex].lecturesList[lectureIndex + 1]
+        );
+      }
+    } catch {
+      console.log("end reached");
+    }
+  };
   return (
     <div>
       <video className="w-full h-auto" src="video.mp4" controls></video>
       <div>
         <div className="text-xl font-medium mt-5 flex justify-between align-center">
           <p>{selectedLecture?.lectureName}</p>
-          <div class="flex justify-between">
-            <button class="bg-green-300 hover:bg-green-400 text-white py-2 px-4 rounded text-sm">
+          <div className="flex justify-between">
+            <button
+              className="bg-green-300 hover:bg-green-400 text-white py-2 px-4 rounded text-sm"
+              onClick={prevLecture}
+            >
               Prev
             </button>
-            <button class="bg-green-300 hover:bg-green-400 text-white py-2 px-4 rounded text-sm ml-4">
+            <button
+              className="bg-green-300 hover:bg-green-400 text-white py-2 px-4 rounded text-sm ml-4"
+              onClick={nextLecture}
+            >
               Next
             </button>
           </div>
