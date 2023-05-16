e19dd5f1fb29117d5861545005c0cc3d4d0113f1
Sarwarhridoy4 <ssarwarhridoy4@gmail.com>
2023-05-16 18:10:43 +0600

    ...

--- src/Pages/DashBoard/AdminPannel/ProgramList/ProgramList.jsx
+++ src/Pages/DashBoard/AdminPannel/ProgramList/ProgramList.jsx
@@ -60,9 +60,11 @@ const ProgramList = () => {
           <input
             id="programName"
             name="programName"
-            {...register("programName", {
-              required: "Program Name is required",
-            })}
+              {...register("programName",
+              //   {
+              // required: "Program Name is required",
+              //   }
+              )}
             class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
             placeholder="Write program name"
             aria-invalid={errors.programName ? "true" : "false"}
