533487d5faac737144492118867cb905c9a7bd31
MD Nazmul Hasa <mdnazmulhasanniloy323@gmai.com>
2023-06-03 22:48:44 -0700

    exercise response modal is complite

--- src/Pages/DashBoard/AdminPannel/ExerciseResponse/ExerciseResponseModal/ExerciseResponseModal.jsx
+++ src/Pages/DashBoard/AdminPannel/ExerciseResponse/ExerciseResponseModal/ExerciseResponseModal.jsx
@@ -1,3 +1,4 @@
+import moment from "moment";
 import React from "react";
 import { BsXCircleFill } from "react-icons/bs";
 
@@ -13,9 +14,96 @@ const ExerciseResponseModal = ({ setOpenModal, data }) => {
             </button>
           </div>
           {/* Contents */}
-          {data?.submissionDetails?.studentEmail}
+          <div className="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-5 p-5">
+            <div className="">
+              <h3 className="text-lg font-semibold text-green-500">
+                Program Name
+              </h3>
+              <h4 className="text-lg ml-2">{data?.program?.programName}</h4>
+            </div>
+            <div className="">
+              <h3 className="text-lg font-semibold text-green-500">
+                Course Name
+              </h3>
+              <h4 className="text-lg ml-2">{data?.course?.courseName}</h4>
+            </div>
+            <div className="">
+              <h3 className="text-lg font-semibold text-green-500">
+                Batch Name
+              </h3>
+              <h4 className="text-lg ml-2">{data?.batch?.batchName}</h4>
+            </div>
+            <div className="">
+              <h3 className="text-lg font-semibold text-green-500">
+                Module Name
+              </h3>
+              <h4 className="text-lg ml-2">{data?.module?.moduleName}</h4>
+            </div>
+            <div className="">
+              <h3 className="text-lg font-semibold text-green-500">
+                Lecture Name
+              </h3>
+              <h4 className="text-lg ml-2">{data?.lecture?.lectureName}</h4>
+            </div>
+            <div className="">
+              <h3 className="text-lg font-semibold text-green-500">
+                Assignment Name
+              </h3>
+              <h4 className="text-lg ml-2">
+                {data?.assignment?.assignmentName}
+              </h4>
+            </div>
+            <div className="">
+              <h3 className="text-lg font-semibold text-green-500">
+                Exercise Name
+              </h3>
+              <h4 className="text-lg ml-2">{data?.exercise?.exerciseName}</h4>
+            </div>
+            <div className="">
+              <h3 className="text-lg font-semibold text-green-500">
+                StartedAt
+              </h3>
+              <h4 className="text-lg ml-2">
+                {moment(data?.submissionDetails?.startedAt).calendar()}
+              </h4>
+            </div>
+            <div className="">
+              <h3 className="text-lg font-semibold text-green-500">
+                FinishedAt
+              </h3>
+              <h4 className="text-lg ml-2">
+                {moment(data?.submissionDetails?.finishedAt).calendar()}
+              </h4>
+            </div>
+            <div className="">
+              <h3 className="text-lg font-semibold text-green-500">
+                Submitted Link
+              </h3>
+              <h4 className="text-lg ml-2">
+                <a
+                  href={data?.submittedLink}
+                  target="_blank"
+                  className="text-blue-700 hover:border-b hover:border-blue-400"
+                >
+                  Link
+                </a>
+              </h4>
+            </div>
+            <div className="">
+              <h3 className="text-lg font-semibold text-green-500">
+                Student Email
+              </h3>
+              <h4 className="text-lg ml-2">
+                {data?.submissionDetails?.studentEmail}
+              </h4>
+            </div>
+          </div>
         </div>
       </div>
+      <div
+        className="opacity-25 fixed inset-0  z-[20000] bg-black"
+        onClick={() => setOpenModal(false)}
+      ></div>
     </>
   );
 };
