bc36bbae53d99aab05a27f9c14a3f3d69cfb862f
JAYENDRA06 <jayendra.awasthi2020@vitstudent.ac.in>
2023-06-12 16:05:44 +0530

    update: design changes in assessment page

--- src/Pages/DashBoard/Assesments/EachAssessment/EachAssessment.jsx
+++ src/Pages/DashBoard/Assesments/EachAssessment/EachAssessment.jsx
@@ -1,13 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import moment from "moment";
-import {
-  BsQuestionCircle,
-  BsFillLockFill,
-  BsListCheck,
-  BsClock,
-  BsArrowRightShort,
-} from "react-icons/bs";
+import { BsQuestionCircle, BsClock, BsArrowRightShort } from "react-icons/bs";
+import { AiFillLock, AiFillUnlock } from "react-icons/ai";
+import textIcon from "../../../../assets/svg/test.svg";
 
 function EachAssessment({ assessment, handleClick, index }) {
   const [available, setAvailable] = useState(false);
@@ -18,75 +14,75 @@ function EachAssessment({ assessment, handleClick, index }) {
     }
   }, []);
   return (
-    <div className=" bg-white border border-gray-200 rounded-2xl shadow dark:bg-gray-800 dark:border-gray-700 ">
-      <div className="p-5">
-        <div className="md:flex justify-between items-center">
-          <div className="">
-            <h1 className="text-lg font-medium tracking-tight text-gray-900 dark:text-white capitalize">
-              {index}. {assessment?.assessmentName}
-            </h1>
-            <h1 className="dark:text-white capitalize mb-2 text-[#4BA25D]">
-              {assessment?.categoryName}
-            </h1>
-          </div>
-          {!available && (
-            <div className="flex mt-2 md:mt-0 gap-1 items-center">
-              <BsFillLockFill size={20} />
-              <h1 className=" text-sm tracking-tight text-gray">
-                Available from
-                <br />
-                {assessment?.scheduledAt
-                  ?.slice(0, 10)
-                  .split("-")
-                  .reverse()
-                  ?.join("-")}
-              </h1>
+    <div className="bg-white border border-gray-200 rounded-2xl shadow hover:shadow-md dark:bg-gray-800 dark:border-gray-700 font-poppins">
+      <div className="p-6">
+        <div className="flex gap-2 justify-between">
+          <div className="flex flex-col gap-4 justify-between">
+            <div>
+              <p className="text-sm md:text-base font-medium capitalize">
+                {index}. {assessment?.assessmentName}
+              </p>
+              <p className="text-xs md:text-sm capitalize">
+                {assessment?.categoryName}
+              </p>
             </div>
-          )}
-        </div>
-        <div className="mt-3">
-          <p className="flex gap-8 items-center mb-3 font-normal font-poppins leading-normal text-gray-700 dark:text-gray-400">
-            <BsQuestionCircle size={20} />
-            <span>Num Questions</span>
-            {assessment?.questions?.length}
-          </p>
-        </div>
-        <div className="mt-3">
-          <div className="flex gap-8 items-center mb-3 font-normal font-poppins leading-normal text-gray-700 dark:text-gray-400">
-            <BsListCheck size={20} />
-            <div className="relative">
-              {/* Displaying Num questions in white to achieve same width */}
-              <span className="text-white">Num Questions</span>
-              <span className="absolute inset-0">Max Mark</span>
+            <div className="text-xs md:text-sm">
+              <p className="flex w-full gap-2 md:gap-6 items-center mb-3 font-normal font-poppins leading-normal text-gray-700 dark:text-gray-400">
+                <BsQuestionCircle size={20} />
+                <span>Total Questions</span>
+                {assessment?.questions?.length}
+              </p>
+
+              <div className="flex w-full gap-2 md:gap-6 items-center mb-3 font-normal font-poppins leading-normal text-gray-700 dark:text-gray-400">
+                <BsClock size={20} />
+                <div className="relative">
+                  <span className=" text-white">Total Questions</span>
+                  <span className="absolute inset-0">Time {"(mins)"}</span>
+                </div>
+                {assessment?.duration}
+              </div>
             </div>
-            {assessment?.questions?.length}
-          </div>
-        </div>
-        <div className="mt-3 md:flex md:flex-row md:justify-between md:items-center">
-          <div className="flex gap-8 items-center mb-3 font-normal font-poppins leading-normal text-gray-700 dark:text-gray-400">
-            <BsClock size={20} />
-            <div className="relative">
-              <span className="text-white">Num Questions</span>
-              <span className="absolute inset-0">Time {"(mins)"}</span>
+            <div>
+              <button
+                onClick={() => handleClick(assessment?._id)}
+                className={`inline-flex items-center mt-2 md:mt-0 px-4 py-2 text-sm font-medium font-poppins text-center ${
+                  available
+                    ? "bg-[#4BA25D] text-white hover:bg-[#5fb370]"
+                    : "bg-[#e4e4e4] text-[#28282899]"
+                } rounded-full focus:ring-4 focus:outline-none focus:ring-[#75AE80] md:text-sm text-xs`}
+                disabled={!available}
+              >
+                Start Test
+                {available ? (
+                  <BsArrowRightShort size={20} className="ml-2" />
+                ) : (
+                  <AiFillLock size={20} color="#28282899" className="ml-2" />
+                )}
+              </button>
             </div>
-            {assessment?.duration}
           </div>
-          <button
-            onClick={() => handleClick(assessment?._id)}
-            className={`inline-flex items-center mt-2 md:mt-0 px-4 py-2 text-sm font-medium font-poppins text-center ${
-              available
-                ? "bg-[#4BA25D] text-white hover:bg-[#5fb370]"
-                : "bg-[#e4e4e4] text-[#28282899]"
-            } rounded-full focus:ring-4 focus:outline-none focus:ring-[#75AE80]`}
-            disabled={!available}
-          >
-            Start
-            {available ? (
-              <BsArrowRightShort size={20} className="ml-1" />
+          <div className="flex flex-col justify-between gap-4 items-end">
+            {!available ? (
+              <div className="flex gap-1 items-center">
+                <AiFillLock size={20} className="text-gray-400" />
+                <h1 className="text-xs md:text-sm text-gray-400 text-right">
+                  Available from
+                  <br />
+                  {assessment?.scheduledAt
+                    ?.slice(0, 10)
+                    .split("-")
+                    .reverse()
+                    ?.join("-")}
+                </h1>
+              </div>
             ) : (
-              <BsFillLockFill size={14} color="#28282899" className="ml-1" />
+              <div className="flex gap-1 items-center">
+                <AiFillUnlock size={20} className="" />
+                <h1 className="text-xs md:text-sm text-right">Available now</h1>
+              </div>
             )}
-          </button>
+            <img src={textIcon} className="w-[70%] md:w-[70%] mb-4" />
+          </div>
         </div>
       </div>
     </div>
